f8098751979488aa8e9a0900f26915356538cb87
Unsatisfied Soul <shovontunvir43@gmail.com>
2022-03-29 21:40:27 +0600

    hovering in sidebar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './Layouts/Home/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+     <BrowserRouter>
+      <Routes>
+      <Route path="/" element={<Home />}/> 
+      </Routes>
+     </BrowserRouter>
     </div>
   );
 }
--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -1,35 +1,40 @@
-import {  faBars, faHouseUser, faXmark } from '@fortawesome/free-solid-svg-icons';
+import {  faBars, faDiagramProject, faEnvelope, faHouseUser, faPen, faUser, faXmark } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import './sidebar.css'
 
 const Sidebar = () => {
    
     return (
         <>
-            <div className="sidebar text-left bg-[#2b2b2b] h-[80vh] text-white flex flex-col mt-16 rounded-r-xl px-4 items-center justify-between transition-all duration-[1000ms] fixed">
+            <div className="sidebar text-left bg-[#2b2b2b] h-[70vh] text-white flex flex-col mt-24 rounded-r-xl px-4 items-center justify-between transition-all duration-[1000ms] fixed">
                 <div className="logo">
-                    <h2 className='text-5xl mt-5'>T</h2>
+                    <h2 className='text-5xl mt-5 logo_name text-yellow-300 font-medium'>T</h2>
                 </div>
                 <ul>
-                    <li className='menu-list flex  text-2xl px-3 py-2 mb-4'>
-                        <FontAwesomeIcon className='ml-24 mr-2' icon={faHouseUser} />
-                        <span className="menu-name">Home</span>
-                    </li>
+                    <Link to="/">
+                        <li className='menu-list flex  text-2xl px-3 py-2 mb-4'>
+                            
+                            <FontAwesomeIcon className='ml-24 mr-2' icon={faHouseUser} />
+                            <span className="menu-name">Home</span>
+                            
+                        </li>
+                    </Link>
                    <li className='menu-list flex  text-2xl px-3 py-2 mb-4'>
-                        <FontAwesomeIcon className='ml-24 mr-2' icon={faHouseUser} />
+                        <FontAwesomeIcon className='ml-24 mr-2' icon={faUser} />
                         <span className="menu-name">About</span>
                     </li>
                    <li className='menu-list flex  text-2xl px-3 py-2 mb-4'>
-                        <FontAwesomeIcon className='ml-24 mr-2' icon={faHouseUser} />
+                        <FontAwesomeIcon className='ml-24 mr-2' icon={faPen} />
                         <span className="menu-name">services</span>
                     </li>
                    <li className='menu-list flex  text-2xl px-3 py-2 mb-4'>
-                        <FontAwesomeIcon className='ml-24 mr-2' icon={faHouseUser} />
+                        <FontAwesomeIcon className='ml-24 mr-2' icon={faDiagramProject} />
                         <span className="menu-name">Projects</span>
                     </li>
                    <li className='menu-list flex  text-2xl px-3 py-2 mb-4'>
-                        <FontAwesomeIcon className='ml-24 mr-2' icon={faHouseUser} />
+                        <FontAwesomeIcon className='ml-24 mr-2' icon={faEnvelope} />
                         <span className="menu-name">Contact</span>
                     </li>
                 </ul>
