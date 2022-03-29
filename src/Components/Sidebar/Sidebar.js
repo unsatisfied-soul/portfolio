9f42db4bb04d5fd138b1845b1e109cd047b2932d
Unsatisfied Soul <shovontunvir43@gmail.com>
2022-03-29 21:17:20 +0600

    sidebar centering

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -1,38 +1,36 @@
 import {  faBars, faHouseUser, faXmark } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useState } from 'react';
+import './sidebar.css'
 
 const Sidebar = () => {
-    const[click,setClick]=useState(false)
-
-    const handleClick = ()=> setClick(!click)
+   
     return (
         <>
-            <button className='text-left' onClick={()=> handleClick()}>{!click? <FontAwesomeIcon icon={faBars}/>: <FontAwesomeIcon icon={faXmark}/>}</button>
-            <div className={!click? "sidebar text-left w-14 bg-black h-[70vh] text-white flex flex-col mt-10 rounded-r-xl px-2 items-center justify-between transition-all duration-[1000ms]" : "sidebar text-left w-36 bg-black h-[70vh] text-white flex flex-col mt-10 rounded-r-xl px-6 items-center justify-between transition-all duration-[1000ms]"}>
+            <div className="sidebar text-left bg-[#2b2b2b] h-[80vh] text-white flex flex-col mt-16 rounded-r-xl px-4 items-center justify-between transition-all duration-[1000ms] fixed">
                 <div className="logo">
                     <h2 className='text-5xl mt-5'>T</h2>
                 </div>
                 <ul>
-                    <li className='flex text-2xl px-3 py-2 mb-4'>
-                        <FontAwesomeIcon className='mr-2' icon={faHouseUser} />
-                        <span className={!click? "hidden" : "block"}>Home</span>
+                    <li className='menu-list flex  text-2xl px-3 py-2 mb-4'>
+                        <FontAwesomeIcon className='ml-24 mr-2' icon={faHouseUser} />
+                        <span className="menu-name">Home</span>
                     </li>
-                    <li className='flex text-2xl px-3 py-2 mb-4'>
-                        <FontAwesomeIcon className='mr-2' icon={faHouseUser} />
-                        <span className={!click? "hidden" : "block"}>About</span>
+                   <li className='menu-list flex  text-2xl px-3 py-2 mb-4'>
+                        <FontAwesomeIcon className='ml-24 mr-2' icon={faHouseUser} />
+                        <span className="menu-name">About</span>
                     </li>
-                    <li className='flex text-2xl px-3 py-2 mb-4'>
-                        <FontAwesomeIcon className='mr-2' icon={faHouseUser} />
-                        <span className={!click? "hidden" : "block"}>Services</span>
+                   <li className='menu-list flex  text-2xl px-3 py-2 mb-4'>
+                        <FontAwesomeIcon className='ml-24 mr-2' icon={faHouseUser} />
+                        <span className="menu-name">services</span>
                     </li>
-                    <li className='flex text-2xl px-3 py-2 mb-4'>
-                        <FontAwesomeIcon className='mr-2' icon={faHouseUser} />
-                        <span className={!click? "hidden" : "block"}>Projects</span>
+                   <li className='menu-list flex  text-2xl px-3 py-2 mb-4'>
+                        <FontAwesomeIcon className='ml-24 mr-2' icon={faHouseUser} />
+                        <span className="menu-name">Projects</span>
                     </li>
-                    <li className='flex text-2xl px-3 py-2 mb-4'>
-                        <FontAwesomeIcon className='mr-2' icon={faHouseUser} />
-                        <span className={!click? "hidden" : "block"}>Contact</span>
+                   <li className='menu-list flex  text-2xl px-3 py-2 mb-4'>
+                        <FontAwesomeIcon className='ml-24 mr-2' icon={faHouseUser} />
+                        <span className="menu-name">Contact</span>
                     </li>
                 </ul>
                 <div className="">
